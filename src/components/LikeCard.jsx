02f2b2142df066102a359a55a2b6841b55c5e423
Inna-decsgr <anjgkwl27@gmail.com>
2025-03-13 16:20:01 +0900

    Modify LikeCard UI and layout for better readability

--- src/components/LikeCard.jsx
+++ src/components/LikeCard.jsx
@@ -1,5 +1,4 @@
 import React  from 'react';
-import { FaHeart } from "react-icons/fa";
 import useLike from '../hooks/useLike';
 import {useNavigate} from 'react-router-dom';
 
@@ -13,14 +12,14 @@ export default function LikeCard({product, product:{id, title, image, price}}) {
 
   return (
     <>
-      <li className='rounded-lg cursor-pointer overflow-hidden transition-all hover:scale-105'>
-        <div className='bg-brand text-white'>
-          <img className='w-full' src={image} alt={title} onClick={() => {navigate(`/products/${id}`, {state:{product}})}}/>
+      <li className='cursor-pointer py-6 border-y border-gray-500 mb-3'>
+        <div className='flex items-center gap-3 px-5'>
+          <img className='w-[150px]' src={image} alt={title} onClick={() => {navigate(`/products/${id}`, {state:{product}})}}/>
           <div className='px-4 py-2'>
             <p className='truncate mt-2'>{title}</p>
-            <div className='flex justify-between items-center'>
-              <p className='font-bold my-2'>₩{price}</p>
-              <FaHeart className='text-lg mr-2 cursor-pointer' onClick={handleremoveLike}/>
+            <div>
+              <p className='mt-2 mb-5 font-semibold text-sm'>{price.toLocaleString()}원</p>
+              <button className='text-xs text-gray-400' onClick={handleremoveLike} >삭제하기</button>
             </div>
           </div>
         </div>
