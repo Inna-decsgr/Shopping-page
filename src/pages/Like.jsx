84253edc43de6f75a115432ff671a7289a630e9c
Inna-decsgr <anjgkwl27@gmail.com>
2025-03-14 18:54:30 +0900

    Refactor Like page layout and add wishlist management buttons

--- src/pages/Like.jsx
+++ src/pages/Like.jsx
@@ -1,21 +1,31 @@
 import React from 'react';
 import useLike from '../hooks/useLike';
 import LikeCard from '../components/LikeCard';
-import { GiHanger } from "react-icons/gi";
 
 export default function Like() {
   const {likeQuery:{data:products}} = useLike();
 
 
   return (
-    <>
-      <GiHanger className='text-9xl w-full my-10'/>
-      <ul className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4 p-6'>
-      {
-        products && products.map(product => <LikeCard key={product.id} product={product}/>)
-      }
-      </ul>
-    </>
+    <div>
+      <p className='text-center text-2xl mt-[120px] mb-[60px]'>WISH LIST</p>
+      {products && products.length > 0 ? (
+        <>
+          <ul className='w-[600px] mx-auto'>
+            {products.map(product => <LikeCard key={product.id} product={product} />)}
+          </ul>
+          <div className='text-center mt-[60px]'>
+            <div className='text-sm pb-3'>
+              <button className='py-2 w-[290px] border-2 border-gray-500 rounded-3xl mr-3'>삭제하기</button>
+              <button className='py-2 w-[290px] border-2 border-gray-500 rounded-3xl'>관심상품 비우기</button>
+            </div>
+            <button className='py-2 w-[600px] border-2 border-gray-500 rounded-3xl text-sm'>장바구니 담기</button>
+          </div>
+        </>
+      ) : (
+        <div className='text-center text-gray-500'>위시리스트가 비어있습니다.</div>
+      )}
+    </div>
   );
 }
 
