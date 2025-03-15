f0878ee8d53f2e1947b2b0dfb78172b43719a820
Inna-decsgr <anjgkwl27@gmail.com>
2025-03-15 22:55:23 +0900

    feat: Update User component and add navigation

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import {useNavigate} from 'react-router-dom';
 
-export default function User({user: {photoURL, displayName}}) {
+
+export default function User({ user: { photoURL, displayName } }) {
+  const navigate = useNavigate();
+
   return (
-    <div className='flex items-center shrink-0'>
-      <img className='w-10 h-10 rounded-full mr-2' src={photoURL} alt={displayName} />
-      <span className='hidden md:block'>{`${displayName}님`}</span>
+    <div className='flex items-center shrink-0 w-[70px]'>
+      {/*<img className='w-10 h-10 rounded-full' src={photoURL} alt={displayName} />*/}
+      <button className='hidden md:block' onClick={() => { navigate(`/products/new`) }}>{`${displayName}님`}</button>
     </div>
   );
 }
