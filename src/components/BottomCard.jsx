5138b08ef6c5dca6d2c85eb4b0f3b2539f9bec73
Inna-decsgr <anjgkwl27@gmail.com>
2025-03-17 16:18:26 +0900

    feat: implement BottomCard component for the home screen

---
+++ src/components/BottomCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function BottomCard() {
+  return (
+    <div className='flex flex-col justify-center h-[300px] text-center'>
+      <p className='text-[45px] pb-4 font-bold'>enjoy your life</p>
+      <p className='text-sm'>Shop smart, shop easy</p>
+      <p className='text-sm'>Enjoy a seamless shopping experience with us</p>
+    </div>
+  );
+}
+
