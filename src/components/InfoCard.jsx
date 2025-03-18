d31b140b1d9688c860e263cb8204d8b191cf6d45
Inna-decsgr <anjgkwl27@gmail.com>
2025-03-18 14:55:40 +0900

    Modify InfoCard layout and adjust image/button spacing

--- src/components/InfoCard.jsx
+++ src/components/InfoCard.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 
 export default function InfoCard() {
   return (
-    <div className='mt-[90px] flex justify-between text-center w-[1000px] mx-auto'>
+    <div className='mt-[90px] flex justify-between text-center gap-[30px] w-[1000px] mx-auto'>
       <div>
-        <img src="image/InfoCard01.jpg" alt="정보 카드01" />
-        <button className='mt-[50px] border py-2 w-[250px] rounded-3xl hover:bg-black hover:text-white transition-all duration-500'>NOTICE</button>
+        <img src="image/InfoCard01.jpg" className='w-[600px]' alt="정보 카드01" />
+        <button className='mt-[30px] border py-2 w-[250px] rounded-3xl hover:bg-black hover:text-white transition-all duration-500'>NOTICE</button>
       </div>
       <div>
-        <img src="image/InfoCard02.jpg" alt="정보 카드02" />
-        <button className='mt-[50px] border py-2 w-[250px] rounded-3xl hover:bg-black hover:text-white transition-all duration-500'>NEW ARRIVAL</button>
+        <img src="image/InfoCard02.jpg" className='w-[600px]' alt="정보 카드02" />
+        <button className='mt-[30px] border py-2 w-[250px] rounded-3xl hover:bg-black hover:text-white transition-all duration-500'>NEW ARRIVAL</button>
       </div>
     </div>
   );
